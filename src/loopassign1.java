4c539646237bb6d57ddca8740ce3d6f4be0b9784
Vijay-2005 <111429863+Vijay-2005@users.noreply.github.com>
2022-12-18 12:44:07 +0530

    Add files via upload

---
+++ src/loopassign1.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+public class loopassign1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//         write a fibonaci series
+//        System.out.println("type n ");
+//
+//        int n = sc.nextInt();
+//        int a = 0 ;
+//        int b = 1;
+//        int c ;
+//
+//        for ( int i = 0; i <  n ; i++){
+//            c = a+ b ;
+//
+//            a = b ;
+//            b = c ;
+//            System.out.print( a + " ");}
+
+//        question 2
+//        System.out.println( " write your number ");
+//        int sum = 0;
+//        while (sc.hasNextInt()){
+//            int num = sc.nextInt();
+//            if (num >= 0){
+//                sum += num;
+//            }else {
+//                sum = -1 ;
+//                break;
+//            }
+//
+//        }
+//        System.out.println(sum);
+
+//        question 3
+// write  a factorial of number
+//        System.out.println("type number ");
+//        int fac = sc.nextInt();
+//        int ans = 1;
+//        for (int i = 1 ;  i <= fac ;  i++){
+//            ans = ans*i ;
+//            System.out.println(ans);
+//        }
+//        System.out.println(ans);
+
+//        question4
+//        System.out.println("type number ");
+//        int n = sc.nextInt();
+//        int x = 0 ;
+//        int temp = 0;
+//        int arm = 0 ;
+//        for (int i = 1   ; i <= n ; i++){
+//            for( i = x ; x>0 ;  ){
+//           temp = x % 10;
+//           temp = (int) Math.pow(temp , 3);
+//           arm += temp;
+//           x = x/10;
+//
+//            }
+//            if ( arm == i ){
+//                System.out.println(i);
+//                System.out.println("this is the armstrong number");
+//            }
+//
+//            }
+
+
+//        answer 4
+//        System.out.println("type number ");
+//        int n = sc.nextInt();
+//        int original = n ;
+//        int temp , arm = 0 ;
+//        while ( n > 0 ){
+//            temp = n % 10 ;
+//            temp = (int) Math.pow(temp , 3);
+//            arm = arm + temp ;
+//            n = n /10;
+//
+//        }
+//if ( arm == original )
+//    System.out.println(" armstrong number ");
+//else System.out.println(" not armstrong number") ;
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
