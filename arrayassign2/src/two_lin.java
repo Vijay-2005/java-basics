8489f8813d6d8429d83d7f4026ce694affea0dda
Vijay-2005 <111429863+Vijay-2005@users.noreply.github.com>
2023-03-05 11:35:31 +0530

    Delete two_lin.java

--- arrayassign2/src/two_lin.java
+++
@@ -1,6 +0,0 @@
-public class two_lin {
-    public static void main(String[] args) {
-        int a= 3, b= 2 ;if ( a< b ){System.out.println( " b is bigger");}else {System.out.println( " a is bigger");}
-
-    }
-}
